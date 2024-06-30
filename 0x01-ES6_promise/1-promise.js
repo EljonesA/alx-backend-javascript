b1ceea85295bde3643cdc5636af654e0cf9b91dc
Eljones <eljones.odongo@gmail.com>
2024-06-30 14:14:09 +0300

    Then & & Catch

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,7 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({ status: 200, body: 'success' });
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
