b4e6aa37418676eacb43e393e7e0de7c88bf860c
Eljones <eljones.odongo@gmail.com>
2024-06-30 20:36:57 +0300

    Class methods

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,6 +1,6 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name != 'string'){
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
