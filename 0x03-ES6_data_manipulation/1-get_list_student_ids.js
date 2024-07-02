69cde770770211f93cc5e0c7d6a1e523f1a21e6c
Eljones <eljones.odongo@gmail.com>
2024-07-02 05:22:08 +0300

    Arrays map method

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(array) {
+  if (Array.isArray(array)) {
+    const ids = array.map(student => student.id);
+    return ids;
+  } else {
+    return [];
+  }
+}
