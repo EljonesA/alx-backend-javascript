5a877fb0d5711a14f3f2c4ae69696eb48795ad73
Eljones <eljones.odongo@gmail.com>
2024-07-05 05:39:48 +0300

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -58,3 +58,14 @@ function executeWork(employee: Director | Teacher): void {
     console.log(employee.workTeacherTasks());
   }
 }
+
+// STRING LITERALS
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return "Teaching Math";
+  } else if (todayClass === 'History') {
+    return "Teaching History";
+  }
+}
