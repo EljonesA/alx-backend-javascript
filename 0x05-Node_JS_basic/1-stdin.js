1ab395d27b351e43c3aa5521992e4c28d3d240ce
Eljones <eljones.odongo@gmail.com>
2024-08-20 20:57:09 +0300

    Update 1-stdin.js1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,10 +6,12 @@ console.log("Welcome to Holberton School, what is your name?");
 process.stdin.on('data', (data) => {
 	const name = data.toString().trim(); // Trim any extra spaces/newlines
 	console.log(`Your name is: ${name}`);
-	process.exit();
+	
+	// Close the input stream if input was piped
+	process.stdin.end();
 });
 
 // handle process exit
-process.on('exit', () => {
+process.stdin.on('end', () => {
 	console.log("This important software is now closing");
 });
